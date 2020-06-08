acac6dc341e1c80a9e1e2c5c4b51c9df1d05c07e
AdamHorsburgh21 <49860618+AdamHorsburgh21@users.noreply.github.com>
2020-06-08 22:40:13 +0100

    Bulk Project Commit
    Forgot to keep up to date with commits for the project and forgot to commit upon completion

---
+++ GUGameAH/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private float moveInput;
+    public float jumpVelocity;
+
+    private Rigidbody2D rb;
+
+    public LayerMask theGround;
+    public Transform groundCheck;
+    bool onTheGround = false;
+
+    private bool facingRight = true;
+
+   // Animator anim;
+   // bool isWalking = false;
+    //bool onGround = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+       // anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //'Ground Check' is created to allow character to jump only when on the ground.
+        onTheGround = Physics2D.Linecast(transform.position, groundCheck.position, theGround);
+        
+        //if statement created so when jump is activated and the character is on the ground, the character will jump
+        if (onTheGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+
+        //Creates horizontal (Left and Right) movement for the player character
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        //if statement created calling to activate Flip function - Makes character face the direction they are moving
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+      /*  if (speed > 0)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+        //anim.SetBool("isWalking", isWalking);
+        //anim.SetBool("onGround", onGround); */
+    }
+
+    void Jump()
+    {
+        //if statement created - If the up arrow/jump key is pressed the character will jump
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    
+    //The player caracter will face the direction they are moving
+    void Flip() 
+    {
+        facingRight = !facingRight;
+        GetComponent<SpriteRenderer>().flipX = !facingRight;
+    }
+
+
+}
---
+++ GUGameAH/Assets/Scripts/SceneChangeDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeDoor : MonoBehaviour
+{
+    public string TargetScene;
+
+   /* private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(TargetScene);
+        }
+        
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(TargetScene);
+        }
+    }
+}
